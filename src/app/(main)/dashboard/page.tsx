fe723b764c77ab17dac38cf4db78ebf45fbc6d2e
96kyle <rhksgnl11@naver.com>
2025-07-24 09:16:19 +0900

    fix: 서버 컴포넌트 from, to 오류 수정

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -11,6 +11,7 @@ import { LoginInfo } from "@/app/types/users/user_type";
 import { Metadata } from "next";
 import { MergedActivity } from "@/app/types/activities/activity_type";
 import DashboardView from "./view";
+import { endOfMonthUTC, startOfMonthUTC } from "@/app/util/date_util";
 // import DashboardView from "./view";
 
 const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
@@ -30,12 +31,12 @@ export const metadata: Metadata = {
 export default async function DashboardPage() {
   const today = new Date();
 
-  const from = today.toISOString();
-  const to = today.toISOString();
+  const from = startOfMonthUTC(today).toISOString();
+  const to = endOfMonthUTC(today).toISOString();
 
   const prevMonth = subMonths(today, 1);
-  const prevFrom = prevMonth.toISOString();
-  const prevTo = prevMonth.toISOString();
+  const prevFrom = startOfMonthUTC(prevMonth).toISOString();
+  const prevTo = endOfMonthUTC(prevMonth).toISOString();
 
   const queryClient = new QueryClient();
 
