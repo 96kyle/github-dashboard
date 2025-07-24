5332773e58525d018f00467d473703a155f33b6b
96kyle <rhksgnl11@naver.com>
2025-07-24 14:49:28 +0900

    fix: histoy time test

--- src/app/util/date_format.ts
+++ src/app/util/date_format.ts
@@ -1,6 +1,7 @@
 import { format, toZonedTime } from "date-fns-tz";
+import { ko } from "date-fns/locale";
 
 export const formatKorean = (date: string | Date, form: string): string => {
-  const kstDate = toZonedTime(new Date(date), "Asia/Seoul");
-  return format(kstDate, form, { timeZone: "Asia/Seoul" });
+  const kstDate = toZonedTime(date, "Asia/Seoul");
+  return format(kstDate, form, { timeZone: "Asia/Seoul", locale: ko });
 };
